57db8cbe155d1aa866e122cbfcf6c660f3e9005a
Dr Anirban Mitra <mitradranirban@gmail.com>
2026-03-22 12:08:30 +0530

    Revert "fix: correct PaintSweepGradient angle variations and missing keys" giving 180 360 Too big error This reverts commit 91f30dfbb0363cfa4893a15759035bb8c1aac71e.

--- src/fontra_compile/builder.py
+++ src/fontra_compile/builder.py
@@ -167,21 +167,8 @@ def _merge_node(nodes, default, model, sources, axisTags, userSpaceLocs):
             "type": ptype,
             "centerX": ms("centerX"),
             "centerY": ms("centerY"),
-            # Scale from turns (Fontra) to degrees (paintcompiler) during merge
-            "startAngle": _merge_scalar(
-                [n.get("startAngle", 0.0) * 360.0 for n in nodes],
-                model,
-                sources,
-                axisTags,
-                userSpaceLocs,
-            ),
-            "endAngle": _merge_scalar(
-                [n.get("endAngle", 0.0) * 360.0 for n in nodes],
-                model,
-                sources,
-                axisTags,
-                userSpaceLocs,
-            ),
+            "startAngle": ms("startAngle"),
+            "endAngle": ms("endAngle"),
             "colorLine": _merge_colorline(
                 [n["colorLine"] for n in nodes], model, sources, axisTags, userSpaceLocs
             ),
